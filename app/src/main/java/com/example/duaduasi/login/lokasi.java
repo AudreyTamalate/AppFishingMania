c1b2279b061ff022661d523792b8c7771f2e58c7
AudreyTamalate <134504764+AudreyTamalate@users.noreply.github.com>
2024-04-21 20:10:28 +0700

    update UI

---
+++ app/src/main/java/com/example/duaduasi/login/lokasi.java
@@ -0,0 +1,18 @@
+package com.example.duaduasi.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class lokasi extends AppCompatActivity {
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lokasi);
+    }
+}
